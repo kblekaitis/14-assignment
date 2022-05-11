9f49b5d30928be12f00ee8d28776875a5e3dd104
kblekaitis <102324204+kblekaitis@users.noreply.github.com>
2022-05-12 01:38:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,158 @@
+
+"use strict"
+var vardas = document.getElementById("vardas-input")
+var pavarde = document.getElementById("pavarde-input")
+var amzius = document.getElementById("amzius-input")
+
+// Parasius kazka i laukeli ir parasius vardas.value gauni laukelio verte. 
+
+var prideti = document.getElementById("prideti")
+var table = document.getElementById("table")
+
+
+// Eilutes pridejimas
+prideti.addEventListener("click", function(e){
+    e.preventDefault()
+    var newElement = document.createElement("tr")
+    if(vardas.value, pavarde.value, amzius.value){
+        newElement.innerHTML = 
+            `
+            <td>${vardas.value}</td>
+            <td>${pavarde.value}</td>
+            <td>${amzius.value}</td>
+            `
+            table.append(newElement)
+            newElement.classList.add("trs")
+            vardas.value = ""
+            pavarde.value = ""
+            amzius.value = ""
+}})
+
+// Paskutines eilutes pasalinimas
+
+var istrintiPas = document.getElementById("istrintiPas")
+
+istrintiPas.addEventListener("click", function(e){
+    e.preventDefault()
+    var trs = document.querySelectorAll(".trs")
+    if(trs.length){
+        trs[trs.length - 1].remove()
+    }
+})
+
+// Pirmos eilutes pasalinimas
+
+var istrintiPir = document.getElementById("istrintiPir")
+
+istrintiPir.addEventListener("click", function(e){
+    e.preventDefault()
+    var trs = document.getElement(".trs")
+    if(trs.length){
+        trs[trs.length - 1].remove()
+    }
+})
+
+/*
+Funkcija 
+*/
+
+function validateForm() {
+  var x = document.forms["vardasForm"]["fVardas"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+
+
+
+prideti.addEventListener("click", function(e){
+    e.preventDefault()
+    var newElement = document.createElement("tr")
+    if(vardas.value, pavarde.value, amzius.value){
+        newElement.innerHTML = 
+            `
+            <td>${vardas.value}</td>
+            <td>${pavarde.value}</td>
+            <td>${amzius.value}</td>
+            `
+            table.append(newElement)
+            newElement.classList.add("trs")
+            vardas.value = ""
+            pavarde.value = ""
+            amzius.value = ""
+}else if(vardas.value, pavarde.value){
+        newElement.innerHTML = 
+            `
+            <td>${vardas.value}</td>
+            <td>${pavarde.value}</td>
+            <td>${amzius.value}</td>
+            `
+            table.append(newElement)
+            newElement.classList.add("trs")
+            vardas.value = ""
+            pavarde.value = ""
+            amzius.value = ""
+}else if(vardas.value){
+        newElement.innerHTML = 
+            `
+            <td>${vardas.value}</td>
+            <td>${pavarde.value}</td>
+            <td>${amzius.value}</td>
+            `
+            table.append(newElement)
+            newElement.classList.add("trs")
+            vardas.value = ""
+            pavarde.value = ""
+            amzius.value = ""
+}else if(pavarde.value, amzius.value){
+        newElement.innerHTML = 
+            `
+            <td>${vardas.value}</td>
+            <td>${pavarde.value}</td>
+            <td>${amzius.value}</td>
+            `
+            table.append(newElement)
+            newElement.classList.add("trs")
+            vardas.value = ""
+            pavarde.value = ""
+            amzius.value = ""
+}else if(vardas.value, amzius.value){
+        newElement.innerHTML = 
+            `
+            <td>${vardas.value}</td>
+            <td>${pavarde.value}</td>
+            <td>${amzius.value}</td>
+            `
+            table.append(newElement)
+            newElement.classList.add("trs")
+            vardas.value = ""
+            pavarde.value = ""
+            amzius.value = ""
+}else if(amzius.value){
+        newElement.innerHTML = 
+            `
+            <td>${vardas.value}</td>
+            <td>${pavarde.value}</td>
+            <td>${amzius.value}</td>
+            `
+            table.append(newElement)
+            newElement.classList.add("trs")
+            vardas.value = ""
+            pavarde.value = ""
+            amzius.value = ""
+}else if(pavarde.value){
+        newElement.innerHTML = 
+            `
+            <td>${vardas.value}</td>
+            <td>${pavarde.value}</td>
+            <td>${amzius.value}</td>
+            `
+            table.append(newElement)
+            newElement.classList.add("trs")
+            vardas.value = ""
+            pavarde.value = ""
+            amzius.value = ""
+}})
+
+
